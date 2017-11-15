10de6aa146771463140b1e8df124014bdc73373d
Zhaoyinga <33551411+Zhaoyinga@users.noreply.github.com>
2017-11-15 23:02:03 +0800

    页面头部2.0
    页面头部2.0

--- header.php
+++ header.php
@@ -18,15 +18,35 @@
         <div class="col-md-8 column">
             <nav class="navbar navbar-default" role="navigation">
                 <div class="navbar-header">
-                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="index.php" target="_top">东软校讯</a>
+                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only"></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="index.php" target="_top">东软校讯</a>
                 </div>
-                
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <?php 
+                    session_start();
+                    if (isset($_SESSION['name'])) {
+                        $name=$_SESSION['name'];
+                        echo '<center>欢迎用户 '.$_SESSION['name'].' 登录！</center>';
+                        echo "
+                       <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                             <a href="login.php" target="_top"><button type="submit" class="btn btn-default">登录</button></a>
-                        </li>
+                            <ul class='nav navbar-nav navbar-right'>
+                                <li>
+                                     <a href='php/exit.php' target='_top'><button type='button' class='btn btn-default'>退出</button></a>
+                                </li>
+                        
+                        ";}
+                     else{
+                        echo "<div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+
+                            <ul class='nav navbar-nav navbar-right'>
+                                <li>
+                                     <a href='login.php' target='_top'><button type='button' class='btn btn-default'>登录</button></a>
+                                </li>";
+                     }   
+                        
+                    
+                 ?>
+                
+ 
                         <li class="dropdown">
                             <a href="register.php" target="_top"><button type="submit" class="btn btn-default">注册</button></a>
                         </li>
